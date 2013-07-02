0043f9b9d19cd036a1907590a1dd633e38e382ed
Kishore Manoharan <kishore.mano87@gmail.com>
2013-07-02 20:16:47 +0530

    Add Full_title

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,41 +2,51 @@ require 'spec_helper'
 
 describe "Static Pages" do
 	describe "Home page" do
-		it "should have the content 'Home'" do
+		it "should have the h1 'Events'" do
 			visit '/static_pages/home'
-			page.should have_content('Home')
+			page.should have_selector('h1', :text => 'Events')
 		end
-
-		it "should have the right title" do
-		visit '/static_pages/home'
-		page.should have_selector('title',
-			:text => "Events | Home")
+		it "should have the base title" do
+			visit '/static_pages/home'
+			page.should have_selector('title',
+				:text => "Events")
+		end
+		it "should not have a custom page title" do
+			visit '/static_pages/home'
+			page.should_not have_selector('title', :text => '| Home')
 		end
 	end
 
 	describe "Help page" do
-		it "should have the content 'Help'" do
+		it "should have the h1 'Help'" do
 			visit '/static_pages/help'
-			page.should have_content('Help')
+			page.should have_selector('h1', :text => 'Help')
 		end
-
-		it "should have the right title" do
-		visit '/static_pages/help'
-		page.should have_selector('title',
-			:text => "Events | Help")
+		it "should have the base title" do
+			visit '/static_pages/help'
+			page.should have_selector('title',
+				:text => "Events")
+		end
+		it "should not have a custom page title" do
+			visit '/static_pages/help'
+			page.should_not have_selector('title', :text => '| Help')
 		end
 	end
 
 	describe "About Us" do
-		it "should have the content 'About Us'" do
+		it "should have the h1 'About Us'" do
 			visit '/static_pages/about'
-			page.should have_content('About Us')
+			page.should have_selector('h1', :text => 'About Us')
+		end
+		it "should have the base title" do
+			visit '/static_pages/about'
+			page.should have_selector('title',
+				:text => "Events")
+		end
+		it "should not have a custom page title" do
+			visit '/static_pages/about'
+			page.should_not have_selector('title', :text => '| About Us')
 		end
 
-		it "should have the right title" do
-		visit '/static_pages/about'
-		page.should have_selector('title',
-			:text => "Events | About Us")
-		end
 	end
 end
